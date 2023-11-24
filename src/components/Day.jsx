03f1bf777826da024ec9cfeea849356f2ed4dcf0
danausenova <aidana99969@gmail.com>
2023-11-24 15:53:22 +0600

    days and tooltip

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,7 +1,43 @@
+import Tooltip from "./Tooltip";
 import React from "react";
 
-const Day = () => {
-  return <div>Day</div>;
+const Day = ({ day, selectedDay, setSelectedDay, contributions }) => {
+  const formattedDay = day.format("YYYY-MM-DD");
+
+  function isSelected() {
+    if (!selectedDay) return false;
+    const format = "DD-MM-YYYY";
+    const selected = selectedDay.format(format);
+    const current = day.format(format);
+
+    return selected === current;
+  }
+
+  function getBgColor() {
+    if (!(formattedDay in contributions)) return "level-1";
+
+    if (contributions[formattedDay] < 10) {
+      return "level-2";
+    } else if (contributions[formattedDay] < 20) {
+      return "level-3";
+    } else if (contributions[formattedDay] < 30) {
+      return "level-4";
+    } else {
+      return "level-5";
+    }
+  }
+
+  return (
+    <Tooltip
+      title={`${contributions[formattedDay] || "No"} contributions`}
+      helperText={day.format("dddd, MMMM DD, YYYY")}
+      afterVisibleChange={(visible) => setSelectedDay(visible ? day : null)}
+    >
+      <div
+        className={`day ${isSelected() ? "selected" : ""} ${getBgColor()}`}
+      ></div>
+    </Tooltip>
+  );
 };
 
 export default Day;
--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import RCTooltip from "rc-tooltip";
+import "rc-tooltip/assets/bootstrap.css";
 
-const Tooltip = () => {
-  return <div>Tooltip</div>;
+const Tooltip = ({ title, helperText, children, ...props }) => {
+  return (
+    <RCTooltip
+      placement="top"
+      trigger={["click"]}
+      overlay={
+        <>
+          <p className="tooltip-text">{title}</p>
+          {helperText && <p className="tooltip-helper-text">{helperText}</p>}
+        </>
+      }
+      {...props}
+    >
+      {children}
+    </RCTooltip>
+  );
 };
 
 export default Tooltip;
